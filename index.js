459e5c7f2314c00d7d25a4b1ff8eb214af2545ec
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-06-07 15:31:29 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//constrain
 const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
+// mongodb connection and api
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Lets Talk server successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// home routes
 app.get("/", (req, res) => {
   res.status(200).json({ message: "server is running", error: false });
 });
